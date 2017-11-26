5d50fd221b6fc4878b6abf3da09c3392d91df508
はよん <33849466+hayeon56@users.noreply.github.com>
2017-11-26 13:18:51 +0900

    Update write.php

--- write.php
+++ write.php
@@ -8,10 +8,12 @@
 
 $title      = $_POST['title'];
 $contents   = $_POST['contents'];
-
-echo $title;
-echo $contents;
 $time       = date("Y-m-d.H:i:s");
+
+@session_start();
+$userId = $_SESSION['userid'];
+$userName = $_SESSION['username'];
+
 //DB연결
 define("HOST","localhost");
 define("USER","root");
@@ -21,10 +23,11 @@ define("TB_NAME","suda_board");
 $db_con = mysqli_connect(HOST,USER,PASSWORD,  DB_NAME);
 
 //쿼리 작성, 전송
-$query = "insert into ".TB_NAME." values(0, 0, 'soraru', 'mahumahu', '$title', '$contents', 0, '$time')";
+$query = "insert into ".TB_NAME." values(0, 0, '$userId', '$userName', '$title', '$contents', 0, '$time')";
 mysqli_query($db_con,$query);
+echo "<script>alert('글작성이 완료 되었습니다')</script>";
+echo "<script>location.href='main.php'</script>";
 
-echo "<a href = 'main.php' ><input type = 'button' value = '메인으로' ></a >";
 
 
 
